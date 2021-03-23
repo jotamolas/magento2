844c346c142c52035d35c2cc6d6bd4d9c3c3b190
root <root@jta-developer.jta>
2021-03-22 22:51:15 -0300

    Boton Arrepentimiento Recaptcha form save

--- app/code/Jotadevs/BotonArrepentimiento/Controller/Carga/Guardar.php
+++ app/code/Jotadevs/BotonArrepentimiento/Controller/Carga/Guardar.php
@@ -15,6 +15,9 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class Guardar extends Boton
 {
+    private static $_siteVerifyUrl = "https://www.google.com/recaptcha/api/siteverify?";
+    private $_secret;
+    private static $_version = "php_1.0";
     protected $transportBuilder;
     protected $inlineTranslation;
     protected $scopeConfig;
@@ -48,40 +51,64 @@ class Guardar extends Boton
     }
     public function execute()
     {
-        $resultRedirect = $this->resultRedirectFactory->create();
-        if (
+        $captcha = $this->getRequest()->getParam('g-recaptcha-response');
+        $secret = "6Lf7uIoaAAAAAFA9XTv30a44TQ-JRZ2-aJ0XkHLz";
+        $response = null;
+        $path = self::$_siteVerifyUrl;
+        $dataC = array(
+            'secret' => $secret,
+            'remoteip' => $_SERVER["REMOTE_ADDR"],
+            'v' => self::$_version,
+            'response' => $captcha
+        );
+        $req = "";
+        foreach ($dataC as $key => $value){
+            $req .= $key . '=' . urlencode(stripslashes($value)) . '&';
+        }
+        $req = substr($req,0,strlen($req)-1);
+        $response = file_get_contents($path . $req);
+        $answer = json_decode($response,true);
+
+        if(trim($answer['success']) == true){
+            $resultRedirect = $this->resultRedirectFactory->create();
+            if (
             !$this->formKeyValidator->validate(
                 $this->getRequest()
             )
-        ) {
+            ) {
+                return $resultRedirect->setRefererUrl();
+            }
+
+            $nombre = $this->getRequest()->getParam('nombre');
+            $apellido = $this->getRequest()->getParam('apellido');
+            $email = $this->getRequest()->getParam('email');
+            $motivo = $this->getRequest()->getParam('motivo');
+            $ciudad = $this->getRequest()->getParam('ciudad');
+            $provincia = $this->getRequest()->getParam('provincia');
+            $fecha = $this->getRequest()->getParam('fecha');
+            $dni = $this->getRequest()->getParam('dni');
+            $identificadorCompra = $this->getRequest()->getParam('identificadorCompra');
+
+            try {
+                $caso = $this->casoFactory->create();
+                $caso->setNombre($nombre)
+                    ->setApellido($apellido)
+                    ->setEmail($email)
+                    ->setMotivo($motivo)
+                    ->setCiudad($ciudad)
+                    ->setProvincia($provincia)
+                    ->setFecha($fecha)
+                    ->setDni($dni)
+                    ->setIdentificadorCompra($identificadorCompra);
+                $caso->save();
+            } catch (Exception $e) {
+                $this->messageManager->addError(__('Error occurred during ticket creation.'));
+            }
             return $resultRedirect->setRefererUrl();
+        }else{
+            return $this->messageManager->addErrorMessage("Error en el captcha");
         }
 
-        $nombre = $this->getRequest()->getParam('nombre');
-        $apellido = $this->getRequest()->getParam('apellido');
-        $email = $this->getRequest()->getParam('email');
-        $motivo = $this->getRequest()->getParam('motivo');
-        $ciudad = $this->getRequest()->getParam('ciudad');
-        $provincia = $this->getRequest()->getParam('provincia');
-        $fecha = $this->getRequest()->getParam('fecha');
-        $dni = $this->getRequest()->getParam('dni');
-        $identificadorCompra = $this->getRequest()->getParam('identificadorCompra');
 
-        try {
-            $caso = $this->casoFactory->create();
-            $caso->setNombre($nombre)
-                ->setApellido($apellido)
-                ->setEmail($email)
-                ->setMotivo($motivo)
-                ->setCiudad($ciudad)
-                ->setProvincia($provincia)
-                ->setFecha($fecha)
-                ->setDni($dni)
-                ->setIdentificadorCompra($identificadorCompra);
-            $caso->save();
-        } catch (Exception $e) {
-            $this->messageManager->addError(__('Error occurred during ticket creation.'));
-        }
-        return $resultRedirect->setRefererUrl();
     }
 }
