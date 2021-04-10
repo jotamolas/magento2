ae21522761621027c974e3431549ccb1ff011119
root <root@jta-developer.jta>
2021-04-09 21:11:13 -0300

    Cambios en JS y Vistas de Inventory In Store

---
+++ app/code/Jotadevs/RedCustoms/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map : {
+        '*' : {
+            'Magento_InventoryInStorePickupFrontend/js/view/store-selector' : 'Jotadevs_RedCustoms/js/view/store-selector'
+        }
+    }
+}
